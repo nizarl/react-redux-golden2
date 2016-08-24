21c10bc4ce6034364793616c957c4f5da00ec924
ctech-NanaOwirodu <Nana.Owirodu@chenmed.com>
2016-08-24 07:19:51 -0400

    Initial files to refactor."

---
+++ ctrls/moduleService.js
@@ -0,0 +1,18 @@
+﻿MainModule.factory('moduleService', [
+        '$http', '$rootScope', '$timeout', function ($http, $rootScope, $timeout) {
+            var baseUrl = 'https://mynotes-api.dev.chenmed.local';
+            //var noteId;
+            //var patientId;
+            $http.defaults.useXDomain = true;
+
+                return {
+                    getPendingNotes: function (callback, errorCallback) {
+                        $http.get(baseUrl + '/doctor/notes/pending').success(callback).error(errorCallback);
+                    },
+                    baseUrl:baseUrl
+                    //noteId:noteId,
+                    //patientId:patientId
+
+                }
+            }
+   ])
