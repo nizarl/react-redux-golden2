a9718238e1c5744bcbc6db92c0ad9eb73bb675b6
Charles <charles.cerecedo@chenmed.com>
2017-06-09 10:00:22 -0400

    add ci.properties

--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,7 @@
     // Register Angular module
     var carePro = angular.module('carepro', ['chenExternalUIComponents']);
 
-    carePro.config(function($stateProvider, $locationProvider, $sceDelegateProvider){
+    carePro.config(function($stateProvider, $locationProvider, $sceDelegateProvider, $urlRouterProvider){
         $locationProvider.html5Mode(true);
 
         $sceDelegateProvider.resourceUrlWhitelist([
@@ -17,6 +17,9 @@
             'https://ux-components-qa.chenmed.local/**',
             'https://ux-components.chenmed.com/**'
         ]);
+
+	$urlRouterProvider.otherwise('/');
+
 	$stateProvider.state('carepro', {
 		name: "carepro",
 		url: "/carepro",
@@ -24,8 +27,11 @@
 	}).state('depression-screening', {
 		name: "depression-screening",
 		url: "/depression-screening",
+		resolve: { myData: function(){
+				return { value: 'simple'}; 
+			}
+		},
 		template:	'<depressionscreening-component></depressionscreening-component>'
-
     	});
     });
 })();
